fa185066706f54f237d2a7e3ad460a8fd4f007a6
wick7034 <wickedguy7034@gmail.com>
2026-03-14 21:39:27 +0530

    test

--- App.tsx
+++ App.tsx
@@ -1,20 +1,36 @@
+import React from 'react';
+import { NavigationContainer, DarkTheme } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+// Screen Imports
+import HomeScreen from './src/screens/HomeScreen';
+import WorkoutScreen from './src/screens/WorkOutScreen';
+import SessionCompleteScreen from './src/screens/SessionCompleteScreen';
+import StatsScreen from './src/screens/StatsScreen';
+import LeaderboardScreen from './src/screens/LeaderboardScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer theme={DarkTheme}>
+        <Stack.Navigator 
+          screenOptions={{ 
+            headerShown: false, 
+            animation: 'fade',
+            contentStyle: { backgroundColor: '#000' }
+          }}
+        >
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Workout" component={WorkoutScreen} />
+          <Stack.Screen name="SessionComplete" component={SessionCompleteScreen} />
+          <Stack.Screen name="Stats" component={StatsScreen} />
+          <Stack.Screen name="Leaderboard" component={LeaderboardScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
